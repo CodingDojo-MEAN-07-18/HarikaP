2edf3893f9a7ed4885e302e261e8d08c8cc675f7
harik <pharika31@gmail.com>
2018-08-20 22:43:36 -0700

    updating file

--- MongoDB/modularization/quoting_dojo_routes/server/config/routes.js
+++ MongoDB/modularization/quoting_dojo_routes/server/config/routes.js
@@ -1,24 +1,16 @@
 const mongoose = require('mongoose');
-const Quote =  mongoose.model('UserQuote');
+const quotes = require('../controllers/quotes.js');
 module.exports = function (app){
     app.get('/', function(req, res) {
-        res.render('index');
+        quotes.index(req,res);
     })
 
     app.post('/quotes', function (req, res){
-      Quote.create(req.body, function(err) {
-          if (err) { console.log(err); }
-          res.redirect('/quotes');
-        })
-
+      quotes.create(req, res);
 
     })
     app.get('/quotes',function(req,res){
-      Quote.find({}, function(err, quotes) {
-       if (err) { console.log(err); }
-       res.render('quotes', { quotes: quotes });
-
-        })
+       quotes.show(req, res);
     })
 
   };
